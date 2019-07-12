6a93a00860ca99f3d250f5f9e7bafc3b12c2b384
Tony C <tonychiesa92@gmail.com>
2019-07-11 20:36:34 -0400

    adding values from submit button to database

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Hello World!");
+
+$(".btn").on("click", function(event){
+    event.preventDefault();
+
+    var name= $("#exampleName").val().trim();
+    var role= $("#exampleRole").val().trim();
+    var date= $("#exampleDate").val().trim();
+    var rate= $("#exampleRate").val().trim();
+
+    database.ref().set({
+        name:name,
+        role:role,
+        date:date,
+        rate:rate
+    });
+});
